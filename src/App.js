03655ec7e8cd0e980dbbb0b099eec6a6d3f781e2
Timur <kamaldinov321@gmail.com>
2023-11-23 20:45:41 +0600

    master

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
-import { useState } from 'react';
-import AirpodsDetect from './AirPods/AirpodsDetect';
 import './App.css';
 
 function App() {
-  const [close,setClose] = useState(false)
-  const [show,setShow] = useState(false)
-  const showHandler = ()=>{
-   setShow(true)
-   setClose(false)
-  }
-  const closeHanlder = ()=>{
-    setClose(true)
-   }
-  return <div className='container'>
-    <button className='show-button' onClick={showHandler}>show</button>
-    {show && 
-    <div className={close ? 'close' : 'pods'}> 
-   <AirpodsDetect onClose={closeHanlder} />;
-    </div>
-  }
-  </div>
+  return <div/>
 }
 
 export default App;
